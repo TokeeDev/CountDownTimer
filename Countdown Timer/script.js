359de2e7b8ea7efef079d1818dfb64c2ca2c63f4
Kwazii <146226859+KwaziiMedia@users.noreply.github.com>
2024-01-13 23:36:27 -0500

    started off some js got stuck ill figure some more out tomorrow

---
+++ Countdown Timer/script.js
@@ -0,0 +1,13 @@
+//input variables
+const hourInput = document.querySelector("#hours");
+const minuteInput = document.querySelector("#minutes");
+const secondInput = document.querySelector("#seconds")
+//gets user input
+hourInput.addEventListener("change", updateUserInput)
+minuteInput.addEventListener("change", updateUserInput)
+secondInput.addEventListener("change", updateUserInput)
+//Countdown Timer Components
+var hours = document.querySelector("#hourTime");
+var minutes = document.querySelector("#minuteTime");
+var seconds = document.querySelector("#secondsTimer")
+
